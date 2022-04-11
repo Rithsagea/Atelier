15cc5c2ddd1d65bf9ff50f282a312492bbfbdff0
Rithsagea Aquadom <rithsagea@gmail.com>
2022-04-11 11:56:04 -0400

    db examples

--- Atelier/src/com/rithsagea/atelier/AtelierRunner.java
+++ Atelier/src/com/rithsagea/atelier/AtelierRunner.java
@@ -6,16 +6,6 @@ public class AtelierRunner {
 		
 		bot.init();
 		
-		//For when MongoDB breaks
-//		AtelierDB db = bot.getDatabase();
-//		User user = db.getUser(171378138041942016l);
-//		Sheet sheet = new Sheet(UUID.fromString("1ced756c-dbc6-4be5-af0f-d6e66beffb0e"));
-//		
-//		user.setLevel(PermissionLevel.ADMINISTRATOR);
-//		user.setSheetId(sheet.getId());
-//		DBTest.initializeLita(sheet);
-//		db.addSheet(sheet);
-		
 		while(bot.isRunning());
 	}
 }
--- Atelier/test/com/rithsagea/test/DBTest.java
+++ Atelier/test/com/rithsagea/test/DBTest.java
@@ -1,58 +1,22 @@
 package com.rithsagea.test;
 
-import java.util.UUID;
-
 import com.rithsagea.atelier.Config;
 import com.rithsagea.atelier.discord.PermissionLevel;
-import com.rithsagea.atelier.dnd.Ability;
-import com.rithsagea.atelier.dnd.Sheet;
 import com.rithsagea.atelier.dnd.User;
 import com.rithsagea.atelier.dnd.database.AtelierDB;
-import com.rithsagea.atelier.dnd.types.spread.PointBuySpread;
 
 public class DBTest {
-	
-	public static void initializeLita(Sheet sheet) {
-		PointBuySpread spread = new PointBuySpread();
-		spread.setScore(Ability.STRENGTH, 10);
-		spread.setScore(Ability.DEXTERITY, 13);
-		spread.setScore(Ability.CONSTITUTION, 10);
-		spread.setScore(Ability.INTELLIGENCE, 13);
-		spread.setScore(Ability.WISDOM, 12);
-		spread.setScore(Ability.CHARISMA, 15);
-		
-		sheet.setName("Lita");
-		sheet.setBaseScores(spread);
-	}
-	
-	public static void printSheet(Sheet sheet) {
-		System.out.println("Name: " + sheet.getName());
-		
-		for(Ability ability : Ability.values() ) {
-			System.out.println("\t" + ability + ": " + sheet.getAbilityScore(ability));
-		}
-	}
-	
 	public static void main(String[] args) {
 		Config config = new Config("config.properties");
 		AtelierDB db = new AtelierDB(config);
 		
-		User user = db.getUser(171378138041942016l);
-		Sheet sheet = db.getSheet(UUID.fromString("1ced756c-dbc6-4be5-af0f-d6e66beffb0e"));
-		
-		for(Sheet s : db.listSheets()) {
-			System.out.println(s.getId());
-		}
-		
-		System.out.println(user.getName() + ": " + user.getId());
-		System.out.println("Level: " + user.getLevel());
-		
-		user.setLevel(PermissionLevel.ADMINISTRATOR);
-		user.setSheetId(sheet.getId());
-		
-		initializeLita(sheet);
-		printSheet(sheet);
+		editDB(db);
 		
 		db.save();
+	}
+	
+	public static void editDB(AtelierDB db) {
+		User user = db.getUser(171378138041942016l); // Rithsagea
+		user.setLevel(PermissionLevel.ADMINISTRATOR);
 	}
 }
