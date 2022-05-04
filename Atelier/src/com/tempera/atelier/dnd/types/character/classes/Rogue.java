25427480440c83f176ec2c75b9a16935d5c69665
Rithsagea Aquadom <rithsagea@gmail.com>
2022-05-04 13:29:25 -0400

    reflectively scan for subtypes

--- Atelier/src/com/tempera/atelier/dnd/types/character/classes/Rogue.java
+++ Atelier/src/com/tempera/atelier/dnd/types/character/classes/Rogue.java
@@ -1,11 +1,13 @@
 package com.tempera.atelier.dnd.types.character.classes;
 
+import com.tempera.atelier.dnd.IndexedItem;
 import com.tempera.atelier.dnd.types.character.CharacterClass;
 import com.tempera.atelier.dnd.types.character.features.HitPointFeature;
 import com.tempera.atelier.dnd.types.character.features.ProficiencyFeature;
 import com.tempera.atelier.dnd.types.enums.Ability;
 import com.tempera.atelier.dnd.types.enums.Equipment;
 
+@IndexedItem("rogue")
 public class Rogue extends CharacterClass {
 	
 	public Rogue() {
