c761c01bc48c3f9e707a16111e89a46bc9b6904e
utinj <utinj123@gmail.com>
2022-05-26 14:24:24 -0400

    AtelierAudioHandler now contains join vc method

--- Atelier/src/com/tempera/atelier/discord/music/AtelierAudioHandler.java
+++ Atelier/src/com/tempera/atelier/discord/music/AtelierAudioHandler.java
@@ -20,8 +20,12 @@ import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
+import net.dv8tion.jda.api.entities.AudioChannel;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class AtelierAudioHandler extends AudioEventAdapter implements AudioLoadResultHandler, AudioSendHandler {
 
@@ -31,48 +35,67 @@ public class AtelierAudioHandler extends AudioEventAdapter implements AudioLoadR
 	private AudioFrame frame;
 	private MessageChannel channel;
 	private boolean looping;
-	
+
 	public AtelierAudioHandler(AudioPlayerManager manager, AudioPlayer player, MessageReceivedEvent event) {
 		this.manager = manager;
 		this.player = player;
 		this.queue = new LinkedBlockingQueue<>();
 		this.looping = false;
-		
+
 		channel = event.getChannel();
-		
+
 		player.addListener(this);
 	}
-	
-	
+
+
 	public boolean toggleLoop() {
 		if (looping)
 			return looping = false;
 		return looping = true;
 	}
+	
+	public AudioChannel joinVc(MessageReceivedEvent event) {
+		if(event.getAuthor().isBot()) return null;
+		
+		Guild guild = event.getGuild();
+		AudioManager manager = guild.getAudioManager();
+		GuildVoiceState state = event.getMember().getVoiceState();
+		if(state == null) return null;
+		
+		AudioChannel channel = state.getChannel();
+		if(channel == null) { event.getChannel().sendMessage("Not in a voice channel!").queue(); return null;}
+		
+		manager.setSendingHandler(this);
+		manager.openAudioConnection(channel);
+		
+		event.getChannel().sendMessage(String.format("Joined channel `[%s]`", channel.getName())).queue();
+		return channel;
+	}
+
 	public void loadTrack(String identifier) {
 		manager.loadItem(identifier, this);
 	}
-	
+
 	public void queue(AudioTrack track) {
 		if(!player.startTrack(track, true)) {
 			queue.offer(track);
 		}
 	}
-	
+
 	public AudioTrack getPlayingTrack() {
 		return player.getPlayingTrack();
 	}
-	
+
 	public void nextTrack() {
 		player.startTrack(queue.poll(), false);
 	}
-	
+
 	public List<AudioTrack> getQueue() {
 		return Collections.unmodifiableList(new ArrayList<>(queue));
 	}
-	
+
 	/// AUDIO EVENT ADAPTER
-	
+
 	@Override
 	public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
 		if (looping)
@@ -81,9 +104,9 @@ public class AtelierAudioHandler extends AudioEventAdapter implements AudioLoadR
 			nextTrack();
 		}
 	}
-	
+
 	/// AUDIO SEND HANDLER
-	
+
 	@Override
 	public boolean canProvide() {
 		frame = player.provide();
@@ -94,23 +117,25 @@ public class AtelierAudioHandler extends AudioEventAdapter implements AudioLoadR
 	public ByteBuffer provide20MsAudio() {
 		return ByteBuffer.wrap(frame.getData());
 	}
-	
+
 	@Override
 	public boolean isOpus() {
 		return true;
 	}
 
-	
+
 	/// AUDIO LOAD RESULT HANDLER
-	
-	
+
+
 	@Override
 	public void trackLoaded(AudioTrack track) {
-		EmbedBuilder eb = new EmbedBuilder();
-		eb.setColor(Color.GREEN);
-		eb.setTitle(String.format("Added - `%s`", track.getInfo().title));
-		channel.sendMessageEmbeds(eb.build()).queue();
-		
+		if(!looping) {
+			EmbedBuilder eb = new EmbedBuilder();
+			eb.setColor(Color.GREEN);
+			eb.setTitle(String.format("Added - `%s`", track.getInfo().title));
+			channel.sendMessageEmbeds(eb.build()).queue();
+		}
+
 		queue(track);
 	}
 
@@ -120,7 +145,7 @@ public class AtelierAudioHandler extends AudioEventAdapter implements AudioLoadR
 		eb.setColor(Color.GREEN);
 		eb.setTitle(String.format("Added %d songs", playlist.getTracks().size()));
 		channel.sendMessageEmbeds(eb.build()).queue();
-		
+
 		playlist.getTracks().forEach(this::queue);
 	}
 
--- Atelier/src/com/tempera/atelier/discord/music/JoinCommand.java
+++ Atelier/src/com/tempera/atelier/discord/music/JoinCommand.java
@@ -35,20 +35,7 @@ public class JoinCommand extends MusicSubCommand{
 	
 	@Override
 	public void execute(AtelierAudioHandler audioHandler, User user, List<String> args, MessageReceivedEvent event) {
-		if(event.getAuthor().isBot()) return;
-		
-		Guild guild = event.getGuild();
-		AudioManager manager = guild.getAudioManager();
-		GuildVoiceState state = event.getMember().getVoiceState();
-		if(state == null) return;
-		
-		AudioChannel channel = state.getChannel();
-		if(channel == null) return;
-		
-		manager.setSendingHandler(audioHandler);
-		manager.openAudioConnection(channel);
-		
-		event.getChannel().sendMessage(String.format("Joined channel `[%s]`", channel.getName())).queue();
+		audioHandler.joinVc(event);
 	}
 
 }
