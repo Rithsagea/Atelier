219b2ef3e86f3c60d84fcce0dcebfb70a1fa055f
Rithsagea Aquadom <rithsagea@gmail.com>
2022-06-02 13:47:34 -0400

    wip starting equipment feature

--- Atelier/src/com/tempera/atelier/dnd/types/character/features/StartingEquipmentFeature.java
+++ Atelier/src/com/tempera/atelier/dnd/types/character/features/StartingEquipmentFeature.java
@@ -1,11 +1,50 @@
 package com.tempera.atelier.dnd.types.character.features;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.tempera.atelier.discord.Menu;
 import com.tempera.atelier.dnd.types.IndexedItem;
 import com.tempera.atelier.dnd.types.character.Attribute;
+import com.tempera.atelier.dnd.types.equipment.Item;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
 
 @IndexedItem("feature-starting-equipment")
 public class StartingEquipmentFeature implements Attribute {
+
+	public class EquipmentOption {
+		private String name;
+		private Item item;
+
+		public EquipmentOption(String name, Item item) {
+			this.name = name;
+			this.item = item;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Item getItem() {
+			return item;
+		}
+	}
+
+	private List<List<EquipmentOption>> options;
+
+	@SafeVarargs
+	public StartingEquipmentFeature(List<EquipmentOption>... options) {
+		this.options = Stream.of(options)
+				.map(Collections::unmodifiableList)
+				.collect(Collectors.toList());
+	}
 
 	@Override
 	public String getName() {
@@ -14,7 +53,33 @@ public class StartingEquipmentFeature implements Attribute {
 
 	@Override
 	public Menu getMenu() {
-		return null;
+		return new StartingEquipmentMenu();
 	}
 
+	private class StartingEquipmentMenu implements Menu {
+		@Override
+		public Message initialize() {
+			MessageBuilder mb = new MessageBuilder();
+			EmbedBuilder eb = new EmbedBuilder();
+
+			eb.setTitle("Starting Equipment");
+			eb.appendDescription("You start with the following items, plus anything provided by your background.");
+			eb.appendDescription("\n\n");
+			eb.appendDescription("Lorem Ipsum");
+
+			mb.setEmbeds(eb.build());
+
+			return mb.build();
+		}
+
+		@Override
+		public void onButtonInteract(ButtonInteractionEvent event) {
+
+		}
+
+		@Override
+		public void onSelectInteract(SelectMenuInteractionEvent event) {
+
+		}
+	}
 }
