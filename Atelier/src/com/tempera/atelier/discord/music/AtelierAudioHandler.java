fb6e02cd13def203a4407827e626fd41ab8aca02
utinj <utinj123@gmail.com>
2022-06-06 13:56:28 -0400

    Added message for adding or looping songs while looping

--- Atelier/src/com/tempera/atelier/discord/music/AtelierAudioHandler.java
+++ Atelier/src/com/tempera/atelier/discord/music/AtelierAudioHandler.java
@@ -148,6 +148,8 @@ public class AtelierAudioHandler extends AudioEventAdapter
 			channel.sendMessageEmbeds(eb.build())
 				.queue();
 		}
+		else
+			channel.sendMessage("Sent song to end of queue").queue();
 
 		queue(track);
 	}
