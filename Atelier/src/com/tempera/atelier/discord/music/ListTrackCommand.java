02b3ec511922169d2db179bf7727bd051b25bc8f
utinj <utinj123@gmail.com>
2022-06-10 08:46:02 -0400

    Added listtrack and newtrack commands

---
+++ Atelier/src/com/tempera/atelier/discord/music/ListTrackCommand.java
@@ -0,0 +1,48 @@
+package com.tempera.atelier.discord.music;
+
+import java.util.List;
+
+import com.tempera.atelier.AtelierBot;
+import com.tempera.atelier.discord.User;
+import com.tempera.atelier.discord.commands.PermissionLevel;
+import com.tempera.atelier.dnd.types.AtelierDB;
+import com.tempera.atelier.dnd.types.Track;
+
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class ListTrackCommand extends MusicSubCommand{
+	private AtelierDB db;
+	
+	public ListTrackCommand(AtelierBot bot) {
+		super(bot.getAudioManager());
+		db = bot.getDatabase();
+	}
+
+	@Override
+	public String getLabel() {
+		return "list";
+	}
+
+	@Override
+	public List<String> getAliases() {
+		return null;
+	}
+
+	@Override
+	public PermissionLevel getLevel() {
+		return PermissionLevel.ADMINISTRATOR;
+	}
+
+	@Override
+	public void execute(AtelierAudioHandler audioHandler, User user, List<String> args, MessageReceivedEvent event) {
+		MessageBuilder b = new MessageBuilder();
+		for (Track track : db.listTracks()) {
+			b.append(track);
+			b.append("\n");
+		}
+		event.getChannel()
+			.sendMessage(b.build())
+			.queue();
+	}
+}
