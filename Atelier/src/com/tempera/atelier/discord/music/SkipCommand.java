a8e8943eceeb7dcec3d40b5e7a268bf380bb8160
utinj <utinj123@gmail.com>
2022-06-15 12:40:47 -0400

    skipcommand can now skip multiple tracks at a time

--- Atelier/src/com/tempera/atelier/discord/music/SkipCommand.java
+++ Atelier/src/com/tempera/atelier/discord/music/SkipCommand.java
@@ -4,6 +4,8 @@ import com.tempera.atelier.discord.User;
 
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 public class SkipCommand extends MusicSubCommand {
 
@@ -14,14 +16,26 @@ public class SkipCommand extends MusicSubCommand {
 	@Override
 	public void execute(AtelierAudioHandler audioHandler, User user, SlashCommandInteractionEvent event) {
 		if (audioHandler.getPlayingTrack() != null) {
+			if (event.getOption("tracks") == null) {
 			event.reply(String.format("Skipping `%s`!",
 					audioHandler.getPlayingTrack()
 						.getInfo().title))
 				.queue();
 			audioHandler.nextTrack();
+			}
+			else {
+				event.reply(String.format("Skipping %d tracks!", event.getOption("tracks").getAsInt())).queue();
+				for (int i = 0; i < event.getOption("tracks").getAsInt(); i++)
+					audioHandler.nextTrack();
+			}
 		} else
 			event.reply("No song to skip!")
 				.queue();
+	}
+	
+	@Override
+	public void addOptions(SubcommandData data) {
+		data.addOption(OptionType.STRING, "tracks", "The amount of tracks to skip", false, false);
 	}
 	
 	@Override
