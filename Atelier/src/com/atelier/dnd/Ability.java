29b76e6ce15765858f7d2eab53a7569e01e8ccc0
Rithsagea Aquadom <rithsagea@gmail.com>
2022-09-01 18:21:20 -0400

    add names to ability

--- Atelier/src/com/atelier/dnd/Ability.java
+++ Atelier/src/com/atelier/dnd/Ability.java
@@ -1,10 +1,27 @@
 package com.atelier.dnd;
 
-public enum Ability {
+import com.atelier.AtelierObject;
+
+public enum Ability implements AtelierObject {
 	STRENGTH,
 	DEXTERITY,
 	CONSTITUTION,
 	INTELLIGENCE,
 	WISDOM,
 	CHARISMA;
+	
+	private String name;
+	
+	private Ability() {
+		name = getProperty("name");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
