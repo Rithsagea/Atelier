7e44fa3a2fb2533957f12eeb5d61c207183aa2ee
Rithsagea <rithsagea@gmail.ccom>
2022-11-12 22:45:48 -0500

    show more information in character info

--- Atelier/src/com/atelier/discord/commands/dnd/embeds/CharacterInfoEmbedBuilder.java
+++ Atelier/src/com/atelier/discord/commands/dnd/embeds/CharacterInfoEmbedBuilder.java
@@ -1,6 +1,7 @@
 package com.atelier.discord.commands.dnd.embeds;
 
 import com.atelier.dnd.Ability;
+import com.atelier.dnd.Skill;
 import com.atelier.dnd.character.AtelierCharacter;
 import com.atelier.util.WordUtil;
 
@@ -14,14 +15,41 @@ public class CharacterInfoEmbedBuilder extends AtelierEmbedBuilder {
 		prefix = "";
 		for(Ability ability : Ability.values()) {
 			content.append(prefix);
+
+			if(character.hasSavingProficiency(ability)) content.append("__");
 			content.append(getMessage("ability")
-					.addAbility(ability)
-					.add("baseScore", character.getBaseScore(ability))
-					.add("abilityScore", character.getAbilityScore(ability))
-					.add("abilityModifier", WordUtil.formatModifier(character.getAbilityModifier(ability)))
-					.get());
+				.addAbility(ability)
+				.add("abilityScore", character.getAbilityScore(ability))
+				.add("abilityModifier", WordUtil.formatModifier(character.getAbilityModifier(ability)))
+				.get());
+			if(character.hasSavingProficiency(ability)) content.append("__");
+
 			prefix = "\n";
 		}
-		addField(getMessage("ability.title").get(), content.toString(), false);
+		addField(getMessage("ability.title").get(), content.toString(), true);
+
+		prefix = "";
+		content.setLength(0);
+		for(Ability ability : Ability.values()) {
+			content.append(prefix);
+			content.append(getMessage("saving")
+				.addAbility(ability)
+				.add("savingModifier", WordUtil.formatModifier(character.getSavingModifier(ability)))
+				.get());
+			prefix = "\n";
+		}
+		addField(getMessage("saving.title").get(), content.toString(), true);
+
+		prefix = "";
+		content.setLength(0);
+		for(Skill skill : Skill.values()) {
+			content.append(prefix);
+			content.append(getMessage("skill")
+				.addSkill(skill)
+				.add("skillModifier", WordUtil.formatModifier(character.getSkillModifier(skill)))
+				.get());
+			prefix = "\n";
+		}
+		addField(getMessage("skill.title").get(), content.toString(), false);
 	}
 }
