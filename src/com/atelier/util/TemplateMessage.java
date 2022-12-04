3ba4a9373c1e6bdb10a41269c8ced54d23af127c
Rithsagea <rithsagea@gmail.ccom>
2022-12-03 21:07:25 -0500

    add null guards

--- src/com/atelier/util/TemplateMessage.java
+++ src/com/atelier/util/TemplateMessage.java
@@ -23,6 +23,14 @@ public class TemplateMessage {
 		tokens = new HashMap<>();
 	}
 	
+	/**
+	 * Uses Object.toString() as the value
+	 */
+	public TemplateMessage add(String token, Object value) {
+		tokens.put(token, value.toString());
+		return this;
+	}
+
 	public TemplateMessage add(String token, String value) {
 		tokens.put(token, value);
 		return this;
@@ -34,6 +42,11 @@ public class TemplateMessage {
 	}
 	
 	public TemplateMessage addUser(String token, AtelierUser user) {
+		if(user == null) {
+			tokens.put(token, "null");
+			return this;
+		}
+
 		tokens.put(token + ".name", user.getName());
 		tokens.put(token + ".id", Long.toString(user.getId()));
 		tokens.put(token, user.toString());
@@ -45,6 +58,11 @@ public class TemplateMessage {
 	}
 	
 	public TemplateMessage addCharacter(String token, AtelierCharacter character) {
+		if(character == null) {
+			tokens.put(token, "null");
+			return this;
+		}
+
 		tokens.put(token + ".id", character.getId().toString());
 		tokens.put(token + ".name", character.getName());
 		tokens.put(token, character.toString());
@@ -56,6 +74,11 @@ public class TemplateMessage {
 	}
 
 	public TemplateMessage addCampaign(String token, Campaign campaign) {
+		if(campaign == null) {
+			tokens.put(token, "null");
+			return this;
+		}
+
 		tokens.put(token + ".id", campaign.getId().toString());
 		tokens.put(token + ".name", campaign.getName());
 		tokens.put(token, campaign.toString());
@@ -67,6 +90,11 @@ public class TemplateMessage {
 	}
 	
 	public TemplateMessage addAbility(String token, Ability ability) {
+		if(ability == null) {
+			tokens.put(token, "null");
+			return this;
+		}
+
 		tokens.put(token, ability.getName());
 		return this;
 	}
@@ -76,6 +104,11 @@ public class TemplateMessage {
 	}
 	
 	public TemplateMessage addSkill(String token, Skill skill) {
+		if(skill == null) {
+			tokens.put(token, "null");
+			return this;
+		}
+
 		tokens.put(token, skill.getName());
 		return this;
 	}
